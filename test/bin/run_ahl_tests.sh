0d477c9765d8fa1f92b28d8bf1e054edc0e70cf2
John Fitzpatrick <jfitz@computer.org>
2016-07-24 12:17:25 -0400

    Modify test scripts to allow for input to programs. Add the bounce.bas program from David Ahl.

---
+++ test/bin/run_ahl_tests.sh
@@ -0,0 +1,15 @@
+echo Removing old directory
+if [ -d tests ] ; then rm -r tests ; fi
+
+echo Creating directory tests
+mkdir tests
+
+echo Running all tests...
+ECODE=0
+
+bash test/bin/run_one_test.sh bounce
+((ECODE+=$?))
+
+echo
+echo Failures: $ECODE
+
