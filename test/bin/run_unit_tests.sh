32f89dfdf7731b976ce5f7b8f0872f4fc43a79f4
John Fitzpatrick <jfitz@computer.org>
2016-07-24 19:58:14 -0400

    Check syntax of FOR statements.

--- test/bin/run_unit_tests.sh
+++ test/bin/run_unit_tests.sh
@@ -99,6 +99,12 @@ bash test/bin/run_one_test.sh fornext_5
 ((ECODE+=$?))
 bash test/bin/run_one_test.sh multi_fornext
 ((ECODE+=$?))
+bash test/bin/run_one_test.sh fornext_no_start
+((ECODE+=$?))
+bash test/bin/run_one_test.sh fornext_no_end
+((ECODE+=$?))
+bash test/bin/run_one_test.sh fornext_no_to
+((ECODE+=$?))
 
 bash test/bin/run_one_test.sh syntax_error_1
 ((ECODE+=$?))
