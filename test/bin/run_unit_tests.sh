c6cb907e8e196c5ae44d8ccbfe7e0512eca94ae6
John Fitzpatrick <jfitz@computer.org>
2016-08-17 09:28:22 -0400

    Move unit tests to their own directories.

--- test/bin/run_unit_tests.sh
+++ test/bin/run_unit_tests.sh
@@ -7,242 +7,240 @@ mkdir tests
 echo Running all tests...
 ECODE=0
 
-bash test/bin/run_one_test.sh remark_1
+bash test/bin/run_unit_test.sh remark_1
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh remark_2
-((ECODE+=$?))
-
-bash test/bin/run_one_test.sh values
-((ECODE+=$?))
-bash test/bin/run_one_test.sh values_2
-((ECODE+=$?))
-bash test/bin/run_one_test.sh multistatement
+bash test/bin/run_unit_test.sh remark_2
 ((ECODE+=$?))
 
-bash test/bin/run_one_test.sh dataread
+bash test/bin/run_unit_test.sh values
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh datarestore
+bash test/bin/run_unit_test.sh values_2
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh dataread_t
-((ECODE+=$?))
-
-bash test/bin/run_one_test.sh input
-((ECODE+=$?))
-bash test/bin/run_one_test.sh input_t
+bash test/bin/run_unit_test.sh multistatement
 ((ECODE+=$?))
 
-bash test/bin/run_one_test.sh ifthen_ge
+bash test/bin/run_unit_test.sh dataread
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh ifthen_gt
+bash test/bin/run_unit_test.sh datarestore
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh ifthen_le
-((ECODE+=$?))
-bash test/bin/run_one_test.sh ifthen_lt
-((ECODE+=$?))
-bash test/bin/run_one_test.sh ifthen_ne
-((ECODE+=$?))
-bash test/bin/run_one_test.sh ifthen_eq
-((ECODE+=$?))
-bash test/bin/run_one_test.sh ifthen_eq_error_1
+bash test/bin/run_unit_test.sh dataread_t
 ((ECODE+=$?))
 
-bash test/bin/run_one_test.sh ifthen_ge_t
+bash test/bin/run_unit_test.sh input
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh ifthen_gt_t
-((ECODE+=$?))
-bash test/bin/run_one_test.sh ifthen_le_t
-((ECODE+=$?))
-bash test/bin/run_one_test.sh ifthen_lt_t
-((ECODE+=$?))
-bash test/bin/run_one_test.sh ifthen_ne_t
-((ECODE+=$?))
-bash test/bin/run_one_test.sh ifthen_eq_t
+bash test/bin/run_unit_test.sh input_t
 ((ECODE+=$?))
 
-bash test/bin/run_one_test.sh print_1
+bash test/bin/run_unit_test.sh ifthen_ge
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh print_2
+bash test/bin/run_unit_test.sh ifthen_gt
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh print_3
+bash test/bin/run_unit_test.sh ifthen_le
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh print_4
+bash test/bin/run_unit_test.sh ifthen_lt
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh print_5
+bash test/bin/run_unit_test.sh ifthen_ne
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh print_6
+bash test/bin/run_unit_test.sh ifthen_eq
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh print_7
-((ECODE+=$?))
-bash test/bin/run_one_test.sh print_t1
+bash test/bin/run_unit_test.sh ifthen_eq_error_1
 ((ECODE+=$?))
 
-bash test/bin/run_one_test.sh let_n
+bash test/bin/run_unit_test.sh ifthen_ge_t
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh letless_n
+bash test/bin/run_unit_test.sh ifthen_gt_t
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh let_t
+bash test/bin/run_unit_test.sh ifthen_le_t
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh let_n_plus_n
+bash test/bin/run_unit_test.sh ifthen_lt_t
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh let_n_plus_v
+bash test/bin/run_unit_test.sh ifthen_ne_t
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh let_n_minus_n
-((ECODE+=$?))
-bash test/bin/run_one_test.sh let_n_times_n
-((ECODE+=$?))
-bash test/bin/run_one_test.sh let_n_divide_n
-((ECODE+=$?))
-bash test/bin/run_one_test.sh let_v_plus_n
-((ECODE+=$?))
-bash test/bin/run_one_test.sh let_v_power_n
-((ECODE+=$?))
-bash test/bin/run_one_test.sh let_parens
-((ECODE+=$?))
-bash test/bin/run_one_test.sh let_n_uni_plus
-((ECODE+=$?))
-bash test/bin/run_one_test.sh let_n_uni_minus
-((ECODE+=$?))
-bash test/bin/run_one_test.sh let_error_1
+bash test/bin/run_unit_test.sh ifthen_eq_t
 ((ECODE+=$?))
 
-bash test/bin/run_one_test.sh two_subscripts
+bash test/bin/run_unit_test.sh print_1
+((ECODE+=$?))
+bash test/bin/run_unit_test.sh print_2
+((ECODE+=$?))
+bash test/bin/run_unit_test.sh print_3
+((ECODE+=$?))
+bash test/bin/run_unit_test.sh print_4
+((ECODE+=$?))
+bash test/bin/run_unit_test.sh print_5
+((ECODE+=$?))
+bash test/bin/run_unit_test.sh print_6
+((ECODE+=$?))
+bash test/bin/run_unit_test.sh print_7
+((ECODE+=$?))
+bash test/bin/run_unit_test.sh print_t1
 ((ECODE+=$?))
 
-bash test/bin/run_one_test.sh gosub_1
+bash test/bin/run_unit_test.sh let_n
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh return_no_gosub
+bash test/bin/run_unit_test.sh letless_n
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh multi_gosub
+bash test/bin/run_unit_test.sh let_t
 ((ECODE+=$?))
-
-bash test/bin/run_one_test.sh fornext_1
+bash test/bin/run_unit_test.sh let_n_plus_n
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh fornext_2
+bash test/bin/run_unit_test.sh let_n_plus_v
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh fornext_3
+bash test/bin/run_unit_test.sh let_n_minus_n
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh fornext_4
+bash test/bin/run_unit_test.sh let_n_times_n
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh fornext_5
+bash test/bin/run_unit_test.sh let_n_divide_n
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh multi_fornext
+bash test/bin/run_unit_test.sh let_v_plus_n
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh fornext_no_start
+bash test/bin/run_unit_test.sh let_v_power_n
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh fornext_no_end
+bash test/bin/run_unit_test.sh let_parens
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh fornext_no_to
+bash test/bin/run_unit_test.sh let_n_uni_plus
 ((ECODE+=$?))
-
-bash test/bin/run_one_test.sh syntax_error_1
+bash test/bin/run_unit_test.sh let_n_uni_minus
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh syntax_error_2
-((ECODE+=$?))
-bash test/bin/run_one_test.sh syntax_error_3
-((ECODE+=$?))
-bash test/bin/run_one_test.sh syntax_error_4
+bash test/bin/run_unit_test.sh let_error_1
 ((ECODE+=$?))
 
-bash test/bin/run_one_test.sh function_int
-((ECODE+=$?))
-bash test/bin/run_one_test.sh function_sin
-((ECODE+=$?))
-bash test/bin/run_one_test.sh function_cos
-((ECODE+=$?))
-bash test/bin/run_one_test.sh function_tan
-((ECODE+=$?))
-bash test/bin/run_one_test.sh function_atn
-((ECODE+=$?))
-bash test/bin/run_one_test.sh function_sgn
-((ECODE+=$?))
-bash test/bin/run_one_test.sh function_exp
-((ECODE+=$?))
-bash test/bin/run_one_test.sh function_rnd_0
+bash test/bin/run_unit_test.sh two_subscripts
 ((ECODE+=$?))
 
-bash test/bin/run_one_test.sh function_trn
+bash test/bin/run_unit_test.sh gosub_1
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh function_idn
+bash test/bin/run_unit_test.sh return_no_gosub
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh function_zer
-((ECODE+=$?))
-bash test/bin/run_one_test.sh function_con
-((ECODE+=$?))
-bash test/bin/run_one_test.sh function_det
-((ECODE+=$?))
-bash test/bin/run_one_test.sh function_inv
-((ECODE+=$?))
-bash test/bin/run_one_test.sh random_0
-((ECODE+=$?))
-bash test/bin/run_one_test.sh random_1
+bash test/bin/run_unit_test.sh multi_gosub
 ((ECODE+=$?))
 
-bash test/bin/run_one_test.sh define
+bash test/bin/run_unit_test.sh fornext_1
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh define_2
+bash test/bin/run_unit_test.sh fornext_2
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh define_error
+bash test/bin/run_unit_test.sh fornext_3
 ((ECODE+=$?))
-
-bash test/bin/run_one_test.sh mat_print_1
+bash test/bin/run_unit_test.sh fornext_4
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh mat_print_2
+bash test/bin/run_unit_test.sh fornext_5
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh mat_print_3
+bash test/bin/run_unit_test.sh multi_fornext
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh mat_print_4
+bash test/bin/run_unit_test.sh fornext_no_start
 ((ECODE+=$?))
-
-bash test/bin/run_one_test.sh mat_let_1
+bash test/bin/run_unit_test.sh fornext_no_end
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh mat_let_uni_plus
-((ECODE+=$?))
-bash test/bin/run_one_test.sh mat_let_uni_minus
+bash test/bin/run_unit_test.sh fornext_no_to
 ((ECODE+=$?))
 
-bash test/bin/run_one_test.sh mat_add_m_m
+bash test/bin/run_unit_test.sh syntax_error_1
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh mat_subtract_m_m
+bash test/bin/run_unit_test.sh syntax_error_2
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh mat_multiply_m_m
+bash test/bin/run_unit_test.sh syntax_error_3
 ((ECODE+=$?))
-
-bash test/bin/run_one_test.sh mat_multiply_m_a
-((ECODE+=$?))
-bash test/bin/run_one_test.sh mat_multiply_a_m
+bash test/bin/run_unit_test.sh syntax_error_4
 ((ECODE+=$?))
 
-bash test/bin/run_one_test.sh mat_add_s_m
+bash test/bin/run_unit_test.sh function_int
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh mat_subtract_s_m
+bash test/bin/run_unit_test.sh function_sin
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh mat_multiply_s_m
+bash test/bin/run_unit_test.sh function_cos
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh mat_divide_s_m
+bash test/bin/run_unit_test.sh function_tan
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh mat_power_s_m
+bash test/bin/run_unit_test.sh function_atn
 ((ECODE+=$?))
-
-bash test/bin/run_one_test.sh mat_add_v_m
+bash test/bin/run_unit_test.sh function_sgn
 ((ECODE+=$?))
-
-bash test/bin/run_one_test.sh mat_add_m_s
+bash test/bin/run_unit_test.sh function_exp
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh mat_subtract_m_s
-((ECODE+=$?))
-bash test/bin/run_one_test.sh mat_multiply_m_s
-((ECODE+=$?))
-bash test/bin/run_one_test.sh mat_divide_m_s
-((ECODE+=$?))
-bash test/bin/run_one_test.sh mat_power_m_s
+bash test/bin/run_unit_test.sh function_rnd_0
 ((ECODE+=$?))
 
-bash test/bin/run_one_test.sh trace
+bash test/bin/run_unit_test.sh function_trn
+((ECODE+=$?))
+bash test/bin/run_unit_test.sh function_idn
+((ECODE+=$?))
+bash test/bin/run_unit_test.sh function_zer
+((ECODE+=$?))
+bash test/bin/run_unit_test.sh function_con
+((ECODE+=$?))
+bash test/bin/run_unit_test.sh function_det
+((ECODE+=$?))
+bash test/bin/run_unit_test.sh function_inv
+((ECODE+=$?))
+bash test/bin/run_unit_test.sh random_0
+((ECODE+=$?))
+bash test/bin/run_unit_test.sh random_1
 ((ECODE+=$?))
 
-bash test/bin/run_one_test.sh celcius_to_fahrenheit
+bash test/bin/run_unit_test.sh define_2
 ((ECODE+=$?))
-bash test/bin/run_one_test.sh celcius_2
+bash test/bin/run_unit_test.sh define_error
+((ECODE+=$?))
+
+bash test/bin/run_unit_test.sh mat_print_1
+((ECODE+=$?))
+bash test/bin/run_unit_test.sh mat_print_2
+((ECODE+=$?))
+bash test/bin/run_unit_test.sh mat_print_3
+((ECODE+=$?))
+bash test/bin/run_unit_test.sh mat_print_4
+((ECODE+=$?))
+
+bash test/bin/run_unit_test.sh mat_let_1
+((ECODE+=$?))
+bash test/bin/run_unit_test.sh mat_let_uni_plus
+((ECODE+=$?))
+bash test/bin/run_unit_test.sh mat_let_uni_minus
+((ECODE+=$?))
+
+bash test/bin/run_unit_test.sh mat_add_m_m
+((ECODE+=$?))
+bash test/bin/run_unit_test.sh mat_subtract_m_m
+((ECODE+=$?))
+bash test/bin/run_unit_test.sh mat_multiply_m_m
+((ECODE+=$?))
+
+bash test/bin/run_unit_test.sh mat_multiply_m_a
+((ECODE+=$?))
+bash test/bin/run_unit_test.sh mat_multiply_a_m
+((ECODE+=$?))
+
+bash test/bin/run_unit_test.sh mat_add_s_m
+((ECODE+=$?))
+bash test/bin/run_unit_test.sh mat_subtract_s_m
+((ECODE+=$?))
+bash test/bin/run_unit_test.sh mat_multiply_s_m
+((ECODE+=$?))
+bash test/bin/run_unit_test.sh mat_divide_s_m
+((ECODE+=$?))
+bash test/bin/run_unit_test.sh mat_power_s_m
+((ECODE+=$?))
+
+bash test/bin/run_unit_test.sh mat_add_v_m
+((ECODE+=$?))
+
+bash test/bin/run_unit_test.sh mat_add_m_s
+((ECODE+=$?))
+bash test/bin/run_unit_test.sh mat_subtract_m_s
+((ECODE+=$?))
+bash test/bin/run_unit_test.sh mat_multiply_m_s
+((ECODE+=$?))
+bash test/bin/run_unit_test.sh mat_divide_m_s
+((ECODE+=$?))
+bash test/bin/run_unit_test.sh mat_power_m_s
+((ECODE+=$?))
+
+bash test/bin/run_unit_test.sh trace
+((ECODE+=$?))
+
+bash test/bin/run_unit_test.sh celcius_to_fahrenheit
+((ECODE+=$?))
+bash test/bin/run_unit_test.sh celcius_2
 ((ECODE+=$?))
 
 echo
