349e7b134d29215d62756c38b41c3491b843948f
John Fitzpatrick <jfitz@computer.org>
2016-08-18 20:00:59 -0400

    Add tests for Coan chapter 3 section 2.

--- test/bin/run_coan_tests.sh
+++ test/bin/run_coan_tests.sh
@@ -58,6 +58,13 @@ bash test/bin/run_coan_test.sh prog-3-1-g
 bash test/bin/run_coan_test.sh prog-3-1-h
 ((ECODE+=$?))
 
+bash test/bin/run_coan_test.sh prog-3-2-a
+((ECODE+=$?))
+bash test/bin/run_coan_test.sh prog-3-2-b
+((ECODE+=$?))
+bash test/bin/run_coan_test.sh prog-3-2-c
+((ECODE+=$?))
+
 echo
 echo Failures: $ECODE
 
