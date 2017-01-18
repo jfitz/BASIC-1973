6949048348c1b68f4ecba2d2cb3792ba262e4a35
John Fitzpatrick <jfitz@computer.org>
2017-01-17 21:09:37 -0800

    Add unit tests for Token classes.

---
+++ unit_test/tc_BooleanConstantToken.rb
@@ -0,0 +1,30 @@
+# tests for BooleanConstantToken class
+
+require 'test/unit'
+
+require_relative '../bin/tokens'
+
+class TestBooleanConstantToken < Test::Unit::TestCase
+
+  def test_simple
+    token = BooleanConstantToken.new('ON')
+
+    assert_equal('ON', token.to_s)
+    assert_equal('ON', token.boolean_constant)
+    assert(!token.keyword?)
+    assert(!token.operator?)
+    assert(!token.separator?)
+    assert(!token.function?)
+    assert(!token.text_constant?)
+    assert(!token.numeric_constant?)
+    assert(token.boolean_constant?)
+    assert(!token.user_function?)
+    assert(!token.variable?)
+    assert(!token.groupstart?)
+    assert(!token.groupend?)
+    assert(!token.whitespace?)
+    assert(token.operand?)
+    assert(!token.statement_separator?)
+  end
+  
+end
---
+++ unit_test/tc_StatementSeparatorToken.rb
@@ -0,0 +1,51 @@
+# tests for StatementSeparatorToken class
+
+require 'test/unit'
+
+require_relative '../bin/tokens'
+
+class TestStatementSeparatorToken < Test::Unit::TestCase
+
+  def test_colon
+    token = StatementSeparatorToken.new(':')
+
+    assert_equal(':', token.to_s)
+    assert_equal(':', token.separator)
+    assert(!token.keyword?)
+    assert(!token.operator?)
+    assert(!token.separator?)
+    assert(!token.function?)
+    assert(!token.text_constant?)
+    assert(!token.numeric_constant?)
+    assert(!token.boolean_constant?)
+    assert(!token.user_function?)
+    assert(!token.variable?)
+    assert(!token.groupstart?)
+    assert(!token.groupend?)
+    assert(!token.whitespace?)
+    assert(!token.operand?)
+    assert(token.statement_separator?)
+  end
+  
+  def test_backslash
+    token = StatementSeparatorToken.new('\\')
+
+    assert_equal('\\', token.to_s)
+    assert_equal('\\', token.separator)
+    assert(!token.keyword?)
+    assert(!token.operator?)
+    assert(!token.separator?)
+    assert(!token.function?)
+    assert(!token.text_constant?)
+    assert(!token.numeric_constant?)
+    assert(!token.boolean_constant?)
+    assert(!token.user_function?)
+    assert(!token.variable?)
+    assert(!token.groupstart?)
+    assert(!token.groupend?)
+    assert(!token.whitespace?)
+    assert(!token.operand?)
+    assert(token.statement_separator?)
+  end
+  
+end
