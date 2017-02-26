9a9b84ad540a3e7a6b53b5273ecaf9e558651550
John Fitzpatrick <jfitz@computer.org>
2017-02-26 15:05:06 -0800

    Change directory structure of CMD tests to hold data and reference files in one directory.

--- test/bin/run_cmd_tests.sh
+++ test/bin/run_cmd_tests.sh
@@ -1,32 +1,20 @@
 TESTROOT=test
 TESTBED=tests
+TESTGROUP=cmd
 
 echo Removing old directory
 if [ -d "$TESTBED" ] ; then rm -r "$TESTBED" ; fi
 
-echo Creating directory tests
+echo Creating directory $TESTBED
 mkdir "$TESTBED"
 
 echo Running all tests...
 ECODE=0
 
-bash "$TESTROOT/bin/run_cmd_test.sh" "$TESTROOT" "$TESTBED" list
-((ECODE+=$?))
-bash "$TESTROOT/bin/run_cmd_test.sh" "$TESTROOT" "$TESTBED" list_range_1
-((ECODE+=$?))
-bash "$TESTROOT/bin/run_cmd_test.sh" "$TESTROOT" "$TESTBED" list_range_2
-((ECODE+=$?))
-bash "$TESTROOT/bin/run_cmd_test.sh" "$TESTROOT" "$TESTBED" list_range_3
-((ECODE+=$?))
-bash "$TESTROOT/bin/run_cmd_test.sh" "$TESTROOT" "$TESTBED" list_range_4
-((ECODE+=$?))
-bash "$TESTROOT/bin/run_cmd_test.sh" "$TESTROOT" "$TESTBED" list_dup_line
-((ECODE+=$?))
-bash "$TESTROOT/bin/run_cmd_test.sh" "$TESTROOT" "$TESTBED" enter_list
-((ECODE+=$?))
-bash "$TESTROOT/bin/run_cmd_test.sh" "$TESTROOT" "$TESTBED" revise_list
-((ECODE+=$?))
+for F in "$TESTROOT/$TESTGROUP"/*; do
+    bash "$TESTROOT/bin/run_cmd_test.sh" "$TESTROOT" "$TESTBED" "$TESTGROUP" ${F##*/}
+    ((ECODE+=$?))
+done
 
 echo
 echo Failures: $ECODE
-
