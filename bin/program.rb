aa2a845cde6fc9a56fcabb2f8621976e6a69edee
John Fitzpatrick <jfitz@computer.org>
2018-10-03 19:26:55 -0700

    Fix defect that allowed options to 'leak' from a running program to the interpreter. Changes made by a running program should not flow back the the interpreter.

--- bin/program.rb
+++ bin/program.rb
@@ -411,6 +411,10 @@ class Program
     end
   end
 
+  def duplicate(o)
+    Marshal.load(Marshal.dump(o))
+  end
+
   public
 
   def preexecute_loop(interpreter)
@@ -482,7 +486,8 @@ class Program
     end
 
     reset_profile_metrics
-    interpreter.run(self, action_flags.clone)
+    flags = duplicate(action_flags)
+    interpreter.run(self, flags)
   end
 
   def profile(args)
