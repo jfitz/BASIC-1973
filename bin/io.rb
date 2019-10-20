128cba761a4b3583cd46542b7bde22c9b0737c2b
John Fitzpatrick <jfitz@computer.org>
2019-10-20 10:20:45 -0700

    Tab the print carriage when comma leads items in PRINT.

--- bin/io.rb
+++ bin/io.rb
@@ -75,7 +75,7 @@ class ConsoleIo
 
     @column = 0
     @last_was_numeric = false
-    @last_was_carriage = false
+    @last_was_tab = false
   end
 
   include Reader
@@ -145,7 +145,7 @@ class ConsoleIo
     end
 
     @last_was_numeric = false
-    @last_was_carriage = false
+    @last_was_tab = false
   end
 
   def print_line(text)
@@ -159,7 +159,7 @@ class ConsoleIo
 
   def tab
     space_after_numeric if @last_was_numeric
-    print_item(' ') if @last_was_carriage
+    print_item(' ') if @last_was_tab
 
     zone_width = $options['zone_width'].value
 
@@ -168,12 +168,11 @@ class ConsoleIo
     end
 
     @last_was_numeric = false
-    @last_was_carriage = true
+    @last_was_tab = true
   end
 
   def semicolon
     space_after_numeric if @last_was_numeric
-    print_item(' ') if @last_was_carriage
 
     zone_width = $options['semicolon_zone_width'].value
 
@@ -182,7 +181,7 @@ class ConsoleIo
     end
 
     @last_was_numeric = false
-    @last_was_carriage = true
+    @last_was_tab = false
   end
 
   def implied
@@ -226,7 +225,7 @@ class ConsoleIo
     newline_delay
     @column = 0
     @last_was_numeric = false
-    @last_was_carriage = true
+    @last_was_tab = true
   end
 
   def newline_when_needed
@@ -271,10 +270,6 @@ class NullOut
     false
   end
 
-  def last_was_carriage
-    false
-  end
-  
   def tab; end
 
   def semicolon; end
@@ -375,11 +370,6 @@ class FileHandler
     set_mode(:print)
   end
 
-  def last_was_carriage
-    # for a file, this function does nothing
-    set_mode(:print)
-  end
-  
   def newline
     set_mode(:print)
     @file.puts
