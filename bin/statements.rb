480870b1f73ec7198ef0be2ffe43a7105786268f
John Fitzpatrick <jfitz@computer.org>
2021-04-27 21:19:50 -0700

    Add the RND2 function.

--- bin/statements.rb
+++ bin/statements.rb
@@ -5951,6 +5951,7 @@ class MatLetStatement < AbstractLetStatement
     interpreter.set_default_args('IDN', l_dims)
     interpreter.set_default_args('RND2', l_dims)
     interpreter.set_default_args('RND2%', l_dims)
+    interpreter.set_default_args('RND2$', l_dims)
     interpreter.set_default_args('ZER2', l_dims)
     interpreter.set_default_args('ZER', l_dims)
 
@@ -5966,6 +5967,7 @@ class MatLetStatement < AbstractLetStatement
     interpreter.set_default_args('IDN', nil)
     interpreter.set_default_args('RND2', nil)
     interpreter.set_default_args('RND2%', nil)
+    interpreter.set_default_args('RND2$', nil)
     interpreter.set_default_args('ZER2', nil)
     interpreter.set_default_args('ZER', nil)
 
