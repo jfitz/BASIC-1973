c14c9cc63b132b893efca85c068df1dfef58dfcf
John Fitzpatrick <jfitz@computer.org>
2021-07-30 15:41:04 -0700

    Remove interpreter as parameter to plot(), print(), and write() functions.

--- bin/expressions.rb
+++ bin/expressions.rb
@@ -739,7 +739,7 @@ class BASICArray < AbstractCompound
     'ARRAY: ' + @values.to_s
   end
 
-  def plot(printer, _interpreter)
+  def plot(printer)
     case @dimensions.size
     when 0
       raise BASICSyntaxError, 'Need dimension in array'
@@ -750,7 +750,7 @@ class BASICArray < AbstractCompound
     end
   end
 
-  def print(printer, _interpreter, formats)
+  def print(printer, formats)
     case @dimensions.size
     when 0
       raise BASICSyntaxError, 'Need dimension in array'
@@ -761,7 +761,7 @@ class BASICArray < AbstractCompound
     end
   end
 
-  def write(printer, _interpreter)
+  def write(printer)
     case @dimensions.size
     when 0
       raise BASICSyntaxError, 'Need dimension in array'
@@ -1027,7 +1027,7 @@ class Matrix < AbstractCompound
     'MATRIX: ' + @values.to_s
   end
 
-  def plot(printer, _interpreter)
+  def plot(printer)
     case @dimensions.size
     when 0
       raise BASICSyntaxError, 'Need dimension in matrix'
@@ -1040,7 +1040,7 @@ class Matrix < AbstractCompound
     end
   end
 
-  def print(printer, _interpreter, formats)
+  def print(printer, formats)
     case @dimensions.size
     when 0
       raise BASICSyntaxError, 'Need dimensions in matrix'
@@ -1053,7 +1053,7 @@ class Matrix < AbstractCompound
     end
   end
 
-  def write(printer, _interpreter)
+  def write(printer)
     case @dimensions.size
     when 0
       raise BASICSyntaxError, 'Need dimensions in matrix'
@@ -2415,7 +2415,7 @@ class ValueExpressionSet < AbstractExpressionSet
     return if compounds.empty?
 
     compound = compounds[0]
-    compound.plot(printer, interpreter)
+    compound.plot(printer)
   end
 
   def compound_print(printer, interpreter, formats)
@@ -2424,7 +2424,7 @@ class ValueExpressionSet < AbstractExpressionSet
     return if compounds.empty?
 
     compound = compounds[0]
-    compound.print(printer, interpreter, formats)
+    compound.print(printer, formats)
   end
 
   def compound_write(printer, interpreter)
@@ -2433,7 +2433,7 @@ class ValueExpressionSet < AbstractExpressionSet
     return if compounds.empty?
 
     compound = compounds[0]
-    compound.write(printer, interpreter)
+    compound.write(printer)
   end
 end
 
