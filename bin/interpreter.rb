c00a7a7dc01638dabe3e83d323826bbf191bd8b0
John Fitzpatrick <jfitz@computer.org>
2021-09-29 17:50:57 -0700

    When computing next statement, skip lines that belong to different functions.

--- bin/interpreter.rb
+++ bin/interpreter.rb
@@ -354,15 +354,19 @@ class Interpreter
       begin
         # run each statement
         # start with the first line number
-        @current_line_stmt_mod = find_first_statement
+        @current_line_stmt_mod = @program.find_first_statement
         @running = true
 
-        execute_step while @running
+        while @running
+          ## puts "BEFOR: #{@running} #{@current_line_stmt_mod}"
+          execute_step
+          ## puts "AFTER: #{@running} #{@current_line_stmt_mod}"
+        end
       rescue Interrupt
         stop_running
-
-        close_all_files
       end
+
+      close_all_files
     else
       errors = @program.errors
       errors.each { |error| @console_io.print_item(error) }
@@ -406,7 +410,7 @@ class Interpreter
     raise(BASICSyntaxError, 'Cannot start CHAIN program') unless
       program_okay?
 
-    @next_line_stmt_mod = find_first_statement
+    @next_line_stmt_mod = @program.find_first_statement
   end
 
   private
@@ -415,22 +419,6 @@ class Interpreter
     @file_handlers.each { |_, fh| fh.close }
     @record_read_variables = {}
     @record_write_variables = {}
-  end
-
-  def find_first_statement
-    # set next line to first statement
-    line_number = @program.first_line_number
-    line = @program.lines[line_number]
-    statements = line.statements
-    modifier = 0
-
-    # start with the modifier, if any
-    unless statements.empty?
-      statement = statements[0]
-      modifier = statement.start_index
-    end
-
-    LineStmtMod.new(line_number, 0, modifier)
   end
 
   public
@@ -445,7 +433,7 @@ class Interpreter
 
     close_all_files
 
-    @next_line_stmt_mod = find_first_statement
+    @next_line_stmt_mod = @program.find_first_statement
   end
 
   def seterrorgoto(line_number)
@@ -498,11 +486,7 @@ class Interpreter
 
     statement.print_trace_info(@trace_out, @current_line_stmt_mod)
 
-    if !statement.part_of_user_function.nil? && !@function_running
-      @trace_out.print_line(' Statement ignored')
-    else
-      statement.execute_a_statement(self, @current_line_stmt_mod)
-    end
+    statement.execute_a_statement(self, @current_line_stmt_mod)
   end
 
   def current_user_function
@@ -522,6 +506,7 @@ class Interpreter
       line_index.nil?
 
     @function_stack.push [function_signature, @current_line_stmt_mod, @next_line_stmt_mod]
+    ## puts "PUSH #{function_signature} #{@current_line_stmt_mod} #{@next_line_stmt_mod}"
     @previous_stack.push @previous_line_indexes
     @previous_line_indexes = []
 
@@ -530,20 +515,21 @@ class Interpreter
     @function_running = true
 
     begin
-      execute_step while @running && @function_running
+      while @running && @function_running
+        execute_step
+      end
     rescue Interrupt
       stop_running
     end
-
-    @previous_line_indexes = @previous_stack.pop
-    _, @current_line_stmt_mod, @next_line_stmt_mod = @function_stack.pop
-
-    # one user-def function may invoke a second
-    @function_running = !@function_stack.empty?
   end
 
   def exit_user_function
-    @function_running = false
+    @previous_line_indexes = @previous_stack.pop
+    function_signature, @current_line_stmt_mod, @next_line_stmt_mod = @function_stack.pop
+    ## puts "POP #{function_signature} #{@current_line_stmt_mod} #{@next_line_stmt_mod}"
+
+    # one user-def function may invoke a second
+    @function_running = !@function_stack.empty?
   end
 
   def execute_debug_command(keyword, args, cmd)
