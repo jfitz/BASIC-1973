0e0942080d596eb6f2a69ec45b9fb7aeeca38fad
John Fitzpatrick <jfitz@computer.org>
2022-02-15 19:17:12 -0800

    FOR/NEXT loop starts with first executable statement, not first statement.

--- bin/program.rb
+++ bin/program.rb
@@ -739,7 +739,8 @@ class Program
 
     statements = line.statements
     stmt = current_line_stmt.statement
-    part_of_user_function = statements[stmt].part_of_user_function
+    statement = statements[stmt]
+    part_of_user_function = statement.part_of_user_function
 
     # find next statement within the current line
     stmt += 1
@@ -788,10 +789,12 @@ class Program
     statement = statements[stmt]
     part_of_user_function = statement.part_of_user_function
 
+    # find next modifier in current statement
     mod = current_line_stmt_mod.index
 
     if mod < statement.last_index
       mod += 1
+
       return LineStmtMod.new(line_number, stmt, mod)
     end
 
@@ -800,7 +803,8 @@ class Program
 
     stmt += 1 while
       stmt < statements.size &&
-      statements[stmt].part_of_user_function != part_of_user_function
+      (statements[stmt].executable != :run ||
+      statements[stmt].part_of_user_function != part_of_user_function)
 
     if stmt < statements.size
       start_mod = statements[stmt].start_index
@@ -822,7 +826,8 @@ class Program
 
       stmt += 1 while
         stmt < statements.size &&
-        statements[stmt].part_of_user_function != part_of_user_function
+        (statements[stmt].executable != :run ||
+        statements[stmt].part_of_user_function != part_of_user_function)
 
       if stmt < statements.size
         start_mod = statements[stmt].start_index
@@ -851,6 +856,7 @@ class Program
 
     if mod < statement.last_index
       mod += 1
+
       return LineStmtMod.new(line_number, stmt, mod)
     end
 
@@ -880,8 +886,8 @@ class Program
 
     stmt += 1 while
       stmt < statements.size &&
-      statements[stmt].part_of_user_function != part_of_user_function &&
-      statements[stmt].executable != :run
+      statements[stmt].executable != :run &&
+      statements[stmt].part_of_user_function != part_of_user_function
 
     if stmt < statements.size
       start_mod = statements[stmt].start_index
@@ -904,7 +910,8 @@ class Program
 
       stmt += 1 while
         stmt < statements.size &&
-        statements[stmt].part_of_user_function != part_of_user_function
+        (statements[stmt].executable != :run ||
+        statements[stmt].part_of_user_function != part_of_user_function)
 
       if stmt < statements.size
         start_mod = statements[stmt].start_index
