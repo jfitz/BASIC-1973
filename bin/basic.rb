3188590ba9de08bf31afde2fba36454d4f65989d
John Fitzpatrick <jfitz@computer.org>
2022-07-08 18:19:21 -0700

    Use two tokenizer lists, shift to the alternate for DATA statements.

--- bin/basic.rb
+++ bin/basic.rb
@@ -157,10 +157,12 @@ class Shell
 
     until @done
       prompt = $options['prompt'].value
+
       if need_prompt
         @console_io.print_item(prompt)
         @console_io.newline if prompt.size > 1
       end
+
       cmd = @console_io.read_line
       need_prompt = process_line_keyboard(cmd)
       need_prompt = true if prompt.size == 1
@@ -543,6 +545,38 @@ def make_interpreter_tokenbuilders(quotes, statement_separators, comment_leads)
   tokenbuilders << TextSymbolTokenBuilder.new if allow_ascii
 
   tokenbuilders << VariableTokenBuilder.new
+  tokenbuilders << ListTokenBuilder.new(%w[TRUE FALSE], BooleanLiteralToken)
+  tokenbuilders << WhitespaceTokenBuilder.new
+end
+
+def make_interpreter_data_tokenbuilders(quotes, statement_separators, comment_leads)
+  tokenbuilders = []
+
+  tokenbuilders << CommentTokenBuilder.new(comment_leads)
+
+  unless statement_separators.empty?
+    tokenbuilders <<
+      ListTokenBuilder.new(statement_separators, StatementSeparatorToken)
+  end
+
+  # operators for negative numeric values
+  colon_file = $options['colon_file'].value
+  un_ops = UnaryOperator.operators(colon_file)
+  tokenbuilders << ListTokenBuilder.new(un_ops, OperatorToken)
+
+  tokenbuilders << BreakTokenBuilder.new
+  tokenbuilders << ListTokenBuilder.new([',', ';'], ParamSeparatorToken)
+  tokenbuilders << TextTokenBuilder.new(quotes)
+
+  allow_hash_constant = $options['allow_hash_constant'].value
+  tokenbuilders << NumberTokenBuilder.new(allow_hash_constant)
+
+  tokenbuilders << IntegerTokenBuilder.new
+  tokenbuilders << NumericSymbolTokenBuilder.new
+
+  allow_ascii = $options['allow_ascii'].value
+  tokenbuilders << TextSymbolTokenBuilder.new if allow_ascii
+
   tokenbuilders << ListTokenBuilder.new(%w[TRUE FALSE], BooleanLiteralToken)
   tokenbuilders << WhitespaceTokenBuilder.new
 end
@@ -950,11 +984,14 @@ console_io = ConsoleIo.new
 tokenbuilders =
   make_interpreter_tokenbuilders(quotes, statement_seps, comment_leads)
 
+data_tokenbuilders =
+  make_interpreter_data_tokenbuilders(quotes, statement_seps, comment_leads)
+
 interpreter = Interpreter.new(console_io)
 interpreter.set_default_args('RND', NumericValue.new(1))
 interpreter.set_default_args('RND%', IntegerValue.new(100))
 interpreter.set_default_args('RND$', NumericValue.new(6))
-program = Program.new(console_io, tokenbuilders)
+program = Program.new(console_io, tokenbuilders, data_tokenbuilders)
 interpreter.program = program
 
 if $options['heading'].value
