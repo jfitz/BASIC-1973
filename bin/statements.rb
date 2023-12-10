2a3e94e290971c9b663339633bd5d587c53df3e5
John Fitzpatrick <jfitz@computer.org>
2023-12-10 15:02:51 -0800

    Extend WhileControl to have 'until' variant.

--- bin/statements.rb
+++ bin/statements.rb
@@ -5954,7 +5954,8 @@ class WhileStatement < AbstractStatement
     raise BASICSyntaxError, 'uninitialized WHILE' if
       @wendstmt_line_stmt.nil?
 
-    while_control = WhileControl.new(@expression, @loopstart_line_stmt_mod)
+    while_control = WhileControl.new(:while, @expression,
+                                     @loopstart_line_stmt_mod)
 
     interpreter.enter_loop(while_control)
 
